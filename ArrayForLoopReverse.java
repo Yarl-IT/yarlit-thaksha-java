aa77f810fcba096ea48d4b1ce14bdb57422c0953
Thaksha-Suresh <tkthaksh@gmail.com>
2026-08-07 10:24:09 +0530

    task 29

---
+++ ArrayForLoopReverse.java
@@ -0,0 +1,16 @@
+class ArrayForLoopReverse{
+	public static void main(String args[]){
+		int[] y;
+		y=new int[5];
+		y[0]=10;
+		y[1]=20;
+		y[2]=30;
+		y[3]=40;
+		y[4]=50;
+		
+		for(int i=4; i>=0; i--){
+			System.out.println(y[i]);
+		}
+	}
+}
+	
